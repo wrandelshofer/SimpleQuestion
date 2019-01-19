6f2afdaf9558d57bccd13722b4f078452e47c6a5
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2019-01-19 12:58:56 +0100

    Updates file headers.

--- src/main/java/ch.randelshofer.simplequestion/ch/randelshofer/scorm/lom/AggregationLevelElement.java
+++ src/main/java/ch.randelshofer.simplequestion/ch/randelshofer/scorm/lom/AggregationLevelElement.java
@@ -1,14 +1,5 @@
 /* @(#)AggregationLevelElement.java
- *
- * Copyright (c) 2001 Werner Randelshofer
- * Staldenmattweg 2, Immensee, CH-6405, Switzerland.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Werner Randelshofer. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Werner Randelshofer.
+ * Copyright © Werner Randelshofer, Switzerland. MIT License.
  */
 
 package ch.randelshofer.scorm.lom;
